e7e911682fad29052a76a075de12e2025b503391
Herman Huang <35003938+strike20023@users.noreply.github.com>
2025-10-14 06:52:47 +0000

    init crawling

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-# %%
-import docker
-client = docker.from_env()
-image_name = "selenium/standalone-chrome:latest"
-image = client.images.pull(image_name)
-client.images.get(image_name)
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium import webdriver
+
+options = ChromeOptions()
+driver = webdriver.Remote(options=options, command_executor="http://localhost:4444")
+driver.get("https://bing.com")
+with open("bing.txt", "w", encoding="utf-8") as f:
+    f.write(driver.page_source)
+driver.quit()
