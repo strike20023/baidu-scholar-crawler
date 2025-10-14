b662c963267941f269eb69fbc6dafb437502643f
Herman Huang <35003938+strike20023@users.noreply.github.com>
2025-10-14 07:11:33 +0000

    更新工作流配置，使用 repository_dispatch 触发，并添加输出上传步骤；在 main.py 中引入 QUERY 环境变量以动态命名输出文件

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+# %%
+import os
+try:
+    query = os.environ['QUERY']
+except KeyError:
+    raise ValueError("环境变量 'QUERY' 未设置，请先配置该变量") from None
+# %%
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium import webdriver
 
 options = ChromeOptions()
 driver = webdriver.Remote(options=options, command_executor="http://localhost:4444")
 driver.get("https://bing.com")
-with open("bing.txt", "w", encoding="utf-8") as f:
+
+# %%
+with open(f"crawled-data-{query}.json", "w", encoding="utf-8") as f:
     f.write(driver.page_source)
-driver.quit()
+# %%
